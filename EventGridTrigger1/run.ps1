e8115490b4158744defe397d140f8cfda61f03bc
Hassan Yamchi <Hassan.Yamchi@encana.com>
2022-09-14 10:25:49 -0600

    initial commit

---
+++ EventGridTrigger1/run.ps1
@@ -0,0 +1,54 @@
+param($eventGridEvent, $TriggerMetadata)
+
+# Extracting information from the event
+$data = $eventGridEvent['data']
+$primeKvName = $data['VaultName']
+$keyName = $data['ObjectName']
+
+# 2nd key vault name, normally this should be a tag in the 1st vault name.
+$secKvName = 'yamchi-key-vault-2'
+
+# A folder to keep backup file
+$backupFolder = "$env:Temp\KeyVaultKeyBackup"
+
+try {
+    # Cleaning up any previous items
+    If ((test-path $backupFolder)) {
+        Remove-Item $backupFolder -Recurse -Force
+    }
+    # Create dir
+    New-Item -ItemType Directory -Force -Path $backupFolder
+
+    # Backup the key
+    Backup-AzKeyVaultKey -VaultName $primeKvName -Name $keyName -OutputFile "$backupFolder\$keyName.keybackup" -Force # to override the existing file
+
+    # Restore to the 2nd vault
+    # if rotation: key needs to be purged first, otherwise error will occur
+    $key = Get-AzKeyVaultKey -VaultName $secKvName -Name $keyName
+
+    if ($key){
+        # To purge, first, we need to delete the key 
+        Remove-AzKeyVaultKey -VaultName $secKvName -Name $keyName -Force -ErrorAction Stop
+
+        # Wait 15 sec for delete to be completed
+        Start-Sleep -s 15
+
+        # Purge
+        Remove-AzKeyVaultKey -VaultName $secKvName -Name $keyName -InRemovedState -Force -ErrorAction Stop # no need for user confirmation
+
+        # Wait 15 sec for purge to be completed
+        Start-Sleep -s 15
+    }
+
+    # Restore the key
+    Restore-AzKeyVaultKey -VaultName $secKvName -InputFile "$backupFolder\$keyName.keybackup" -ErrorAction Stop
+
+    If ((test-path $backupFolder)) {
+        Remove-Item $backupFolder -Recurse -Force
+    }
+} catch {
+    Write-Host "the following error occured: $($PSItem.ToString())"
+    $PSItem.InvocationInfo | Format-List *
+}
+
+
